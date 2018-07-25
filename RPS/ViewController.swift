d1b9aa379e4bf945263f7716e3bdc447f2a063d6
John Green <jpgreen@johns-imac.local>
2018-07-24 20:59:11 -0400

    Initial Commit

---
+++ RPS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RPS
+//
+//  Created by John Green on -07-242018.
+//  Copyright © 2018 John P. Green. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
